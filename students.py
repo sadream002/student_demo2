1e11c233d59b91178bbdee9914bdd1f45d7ee7ef
sadream002 <haobin9527@qq.com>
2025-09-17 15:29:57 +0800

    add students.py

---
+++ students.py
@@ -0,0 +1,7 @@
+students = [
+    {"name": "zhangsan", "student_id": "20250001"},
+    {"name": "lisi", "student_id": "20250002"},
+]
+
+for student in students:
+    print(f"Name: {student['name']}, Student ID: {student['student_id']}")
